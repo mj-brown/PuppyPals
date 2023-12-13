0a668f8f35ff6976ab6a82e42c937154b9715ed7
mj-brown <brown8mj5@gmail.com>
2023-12-12 23:08:27 -1000

    imported puppyList into app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from "react"
+import { puppyList } from "./data.js"
 
+function App() {
+
+  const [puppies, setPuppies] = useState(puppyList);
+  
+  console.log(puppies);
+  
   return (
     <div>
-        
+      { 
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
     </div>
-  )
+  );
 }
 
 export default App
